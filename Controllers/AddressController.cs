6fe0d47c41c008db97fb5fcb3503ec7e62a9f66b
saneej <c.saneej@hotmail.com>
2019-04-19 18:47:56 +0100

    Initial Commit 2

--- Controllers/AddressController.cs
+++ Controllers/AddressController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FLoan.System.Web.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/address")]
     public class AddressController : Controller
     {
 
