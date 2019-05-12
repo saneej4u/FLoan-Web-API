5a6bfbeb7d4d60d450e6d39895d423ee87bece85
saneej <c.saneej@hotmail.com>
2019-05-12 22:19:19 +0100

    Transaction Type Id

--- Dtos/TransactionDto.cs
+++ Dtos/TransactionDto.cs
@@ -7,6 +7,7 @@ namespace FLoan.System.Web.API.Dtos
         public decimal CurrentBalance { get; set; }
         public decimal AmountPaid { get; set; }
         public DateTime DateTimeCreated { get; set; }
+        public int TransactionTypeId { get; set; }
 
         public int AgreementId { get; set; }
 
